f91c1a12cc2b0994c42a902723c2c1990370ee09
Danilo Silva Pereira <dansilva41@hotmail.com>
2017-09-20 18:11:02 -0300

    ADD: UsuarioDAO with methods

---
+++ src/main/java/br/com/sitedoph/uniph/infraestrutura/dao/impl/UsuarioDAO.java
@@ -0,0 +1,50 @@
+package br.com.sitedoph.uniph.infraestrutura.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.sitedoph.uniph.dominio.entidade.Usuario;
+import br.com.sitedoph.uniph.infraestrutura.dao.GenericDAO;
+
+public class UsuarioDAO {
+
+	private final GenericDAO<Usuario, Long> DAO;
+
+	public UsuarioDAO(EntityManager entityManager) {
+
+		DAO = new GenericDAOHibernate<>(entityManager, Usuario.class);
+
+	}
+
+	public Usuario buscarPorId(Long id) {
+		return DAO.buscarPorId(id);
+	}
+
+	public Usuario buscarPorLoginESenha(String login, String senha) {
+
+		Usuario exemplo = new Usuario();
+		exemplo.setLogin(login);
+		exemplo.setSenha(senha);
+
+		List<Usuario> lista = DAO.buscarPorExemplo(exemplo, null);
+
+		if (lista != null && !lista.isEmpty()) {
+			return lista.get(0);
+		} else {
+			return null;
+		}
+	}
+
+	public List buscarTodos() {
+		return DAO.buscarTodos();
+	}
+
+	public void excluir(Usuario usuario) {
+		DAO.excluir(usuario);
+	}
+
+	public Usuario salvarOuAtualizar(Usuario usuario) {
+		return DAO.salvarOuAtualizar(usuario);
+	}
+}
