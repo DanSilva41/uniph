f0b4c08f9a85d21e39a87535dbc754f872bb4abb
Danilo Silva Pereira <dansilva41@hotmail.com>
2017-09-26 03:29:20 -0300

    ADD: Template for Tests - DisciplinaDAOTest (OK)

---
+++ src/test/java/br/com/sitedoph/uniph/tests/BaseTest.java
@@ -0,0 +1,17 @@
+package br.com.sitedoph.uniph.tests;
+
+import org.junit.BeforeClass;
+
+import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
+
+/**
+ * CreatedBy danilosilva
+ */
+public class BaseTest {
+	protected static final String VALID = "valid";
+	
+	@BeforeClass
+	public static void setUp() {
+		FixtureFactoryLoader.loadTemplates("br.com.sitedoph.uniph.dominio.entidade.templateloader");
+	}
+}
