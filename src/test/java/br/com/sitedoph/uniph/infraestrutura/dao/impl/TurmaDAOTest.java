9567a2c268b3556f45eb9f0171d6f1073b79505d
Danilo Silva Pereira <dansilva41@hotmail.com>
2017-09-26 10:10:05 -0300

    Corrigindo os testes

--- src/test/java/br/com/sitedoph/uniph/infraestrutura/dao/impl/TurmaDAOTest.java
+++ src/test/java/br/com/sitedoph/uniph/infraestrutura/dao/impl/TurmaDAOTest.java
@@ -2,12 +2,13 @@ package br.com.sitedoph.uniph.infraestrutura.dao.impl;
 
 import javax.persistence.EntityManager;
 
-import org.junit.Test;
+import org.hibernate.Session;
 
 import br.com.sitedoph.uniph.dominio.entidade.Turma;
 import br.com.sitedoph.uniph.infraestrutura.persistencia.JPAUtil;
 import br.com.sitedoph.uniph.tests.BaseTest;
 import br.com.six2six.fixturefactory.Fixture;
+import br.com.six2six.fixturefactory.processor.HibernateProcessor;
 
 public class TurmaDAOTest extends BaseTest {
 
@@ -16,14 +17,18 @@ public class TurmaDAOTest extends BaseTest {
 
 		EntityManager em = JPAUtil.getEntityManager();
 		TurmaDAO dao = new TurmaDAO(em);
-		
+
 		// Para testes
 		AlunoDAO daoAluno = new AlunoDAO(em);
 		DisciplinaDAO daoDisciplina = new DisciplinaDAO(em);
 
-		Turma sala = Fixture.from(Turma.class).gimme(VALID);
+		// Gera e atribui os valores aos campos do objeto e 
+		// invoca o processo para persistir os objetos relacionados
+		// É usado quando o objeto possui relacionamentos. == Evita objetos Transients ==
+		Turma sala = Fixture.from(Turma.class).uses(new HibernateProcessor((Session) em.getDelegate())).gimme(VALID);
+
 		Turma turmaPorDescricao = dao.buscarPorDescricao(sala.getDescricao());
-		
+
 		em.getTransaction().begin();
 		if (turmaPorDescricao != null) {
 			dao.excluir(turmaPorDescricao);
@@ -32,7 +37,7 @@ public class TurmaDAOTest extends BaseTest {
 
 		sala.setAlunos(daoAluno.buscarTodos());
 		sala.setDisciplinas(daoDisciplina.buscarTodos());
-		
+
 		em.getTransaction().begin();
 		sala = dao.salvarOuAtualizar(sala);
 		em.getTransaction().commit();
