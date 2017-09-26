a2d1bf4f2cb7ebcfc7fa914b1c76b9f46e56f2c8
Danilo Silva Pereira <dansilva41@hotmail.com>
2017-09-26 18:24:26 -0300

    ADD: cadastro, listagem e remoção de usuários

--- src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/UsuarioBean.java
+++ src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.sitedoph.uniph.aplicacao.managedsbeans;
 
+import java.util.Collection;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.sitedoph.uniph.dominio.entidades.Usuario;
@@ -7,20 +9,39 @@ import br.com.sitedoph.uniph.dominio.services.UsuarioService;
 
 @ManagedBean
 public class UsuarioBean {
-	
+
 	private Usuario usuario = new Usuario();
-	
-	private UsuarioService 	service = new UsuarioService();
+	private Collection<Usuario> usuarios;
+
+	private UsuarioService service = new UsuarioService();
 
 	public void gravar() {
-		
+
 		System.out.println(usuario);
-		
+
 		service.salvarOuAtualizar(usuario);
-		
 		usuario = new Usuario();
+		// Atribuindo à lista de UsuarioBean
+		usuarios = service.buscarTodos();
 	}
-	
+
+	public void remover(Usuario usuario) {
+
+		System.out.println(usuario);
+
+		service.excluir(usuario);
+		usuarios = service.buscarTodos();
+	}
+
+	public Collection<Usuario> getUsuarios() {
+
+		if (usuarios == null) {
+			usuarios = service.buscarTodos();
+		}
+
+		return usuarios;
+	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -28,5 +49,5 @@ public class UsuarioBean {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
+
 }
