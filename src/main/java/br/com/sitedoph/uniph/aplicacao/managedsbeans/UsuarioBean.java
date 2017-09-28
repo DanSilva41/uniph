800033607d9d11f40798f99df491f73c03fbe5af
Danilo Silva Pereira <dansilva41@hotmail.com>
2017-09-28 11:06:24 -0300

    ADD: CRUD de usuário finalizado (with Ajax)

--- src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/UsuarioBean.java
+++ src/main/java/br/com/sitedoph/uniph/aplicacao/managedsbeans/UsuarioBean.java
@@ -1,14 +1,19 @@
 package br.com.sitedoph.uniph.aplicacao.managedsbeans;
 
+import java.io.Serializable;
 import java.util.Collection;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
 
 import br.com.sitedoph.uniph.dominio.entidades.Usuario;
 import br.com.sitedoph.uniph.dominio.services.UsuarioService;
 
+@ViewScoped
 @ManagedBean
-public class UsuarioBean {
+public class UsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private Usuario usuario = new Usuario();
 	private Collection<Usuario> usuarios;
@@ -17,18 +22,12 @@ public class UsuarioBean {
 
 	public void gravar() {
 
-		System.out.println(usuario);
-
 		service.salvarOuAtualizar(usuario);
-		usuario = new Usuario();
-		// Atribuindo à lista de UsuarioBean
+		limpar();
 		usuarios = service.buscarTodos();
 	}
 
 	public void remover(Usuario usuario) {
-
-		System.out.println(usuario);
-
 		service.excluir(usuario);
 		usuarios = service.buscarTodos();
 	}
@@ -40,6 +39,14 @@ public class UsuarioBean {
 		}
 
 		return usuarios;
+	}
+
+	public void limpar() {
+		usuario = new Usuario();
+	}
+
+	public void cancelar() {
+		limpar();
 	}
 
 	public Usuario getUsuario() {
